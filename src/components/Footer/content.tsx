b1a789a8bb132dc49b4a052499fb5c054f306d03
freewindias <freewindias6@gmail.com>
2025-03-15 20:46:17 +0530

    6

--- src/components/Footer/content.tsx
+++ src/components/Footer/content.tsx
@@ -1,54 +1,61 @@
-import Image from 'next/image'
-import React from 'react'
-import rodiasLogo from '@/assets/images/rodias.jpeg'
+import Image from "next/image";
+import React from "react";
+import rodiasLogo from "@/assets/images/rodias.jpeg";
+import Link from "next/link";
 
 export default function Content() {
-    return (
-        <div className='bg-[#4E4E5A] py-8 px-12 h-full w-full flex flex-col justify-between'>
-            <Section1 />
-            <Section2 />
-        </div>
-    )
+  return (
+    <div className="bg-[#4E4E5A] py-8 px-12 h-full w-full flex flex-col justify-between">
+      <Section1 />
+      <Section2 />
+    </div>
+  );
 }
 
 const Section1 = () => {
-    return (
-        <div>
-            {/* <Nav /> */}
-        </div>
-    )
-}
+  return <div>{/* <Nav /> */}</div>;
+};
 
 const Section2 = () => {
-    return (
-        <div className='flex justify-between items-end'>
-            <h1 className='text-[14vw] leading-none mt-10 text-[#fff] tracking-tighter pointer-events-none'>I-Fineart</h1>
-            <div className=''>
-                <div className='flex gap-6 mb-20 text-white'>
-                    <div>
-                        <nav>
-                            <li>Home</li>
-                            <li>Home</li>
-                            <li>Home</li>
-                        </nav>
-                    </div>
-                    <div>
-                        <nav>
-                            <li>Home</li>
-                            <li>Home</li>
-                            <li>Home</li>
-                        </nav>
-                    </div>
-                </div>
-                <div className='flex gap-2 items-center mb-4'>
-                    <p className='text-[#fff]'>Designed By</p>
-                    <Image src={rodiasLogo} alt='rodias' width={150} height={150} />
-                </div>
-                <p className='text-[#fff]'>© 2024 RODIAS IT Solutions. All rights reserved.</p>
+  return (
+    <footer>
+      <div className="flex justify-between items-end">
+        <h1 className="text-[14vw] leading-none mt-10 text-[#fff] tracking-tighter pointer-events-none">
+          I-Fineart
+        </h1>
+        <div className="">
+          <div className="flex gap-6 mb-20 text-white">
+            <div>
+              <nav className="flex flex-col">
+                <Link href="">Home</Link>
+                <Link href="">About</Link>
+                <Link href="">Exhibition</Link>
+                <Link href="">Collection</Link>
+                <Link href="">Store</Link>
+                <Link href="">Contact</Link>
+              </nav>
             </div>
+            <div>
+              <nav className="flex flex-col">
+                <Link href="">Facebook</Link>
+                <Link href="">Youtube</Link>
+                <Link href="">Twitter(X)</Link>
+                <Link href="">Pinterest</Link>
+              </nav>
+            </div>
+          </div>
+          <div className="flex gap-2 items-center mb-4">
+            <p className="text-[#fff]">Designed By</p>
+            <Image src={rodiasLogo} alt="rodias" width={150} height={150} />
+          </div>
+          <p className="text-[#fff]">
+            © 2024 RODIAS IT Solutions. All rights reserved.
+          </p>
         </div>
-    )
-}
+      </div>
+    </footer>
+  );
+};
 
 // const Nav = () => {
 //     return (
@@ -61,7 +68,7 @@ const Section2 = () => {
 //                     <p>Our Mission</p>
 //                     <p>Contact Us</p>
 //                 </div>
-                
+
 //             </div>
 //         </div>
 //     )
