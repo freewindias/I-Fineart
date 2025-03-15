eee8243cf9eceddc864d646c674a584aec2fe62b
freewindias <freewindias6@gmail.com>
2025-03-15 22:30:38 +0530

    9

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -5,8 +5,7 @@ import Content from './content';
 
 export default function Footer() {
     return (
-        <div 
-            className='relative h-[400px]' style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}>
+        <div className='relative h-[400px]' style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}>
             <div className='relative h-[calc(100vh+400px)] -top-[100vh]'>
                 <div className='h-[400px] sticky top-[calc(100vh-400px)]'>
                     <Content />
