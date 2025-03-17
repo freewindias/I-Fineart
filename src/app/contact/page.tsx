4a38c22cc4c612f69f4833296f1821263ba147d0
freewindias <freewindias6@gmail.com>
2025-03-17 17:06:33 +0530

    27

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='mt-20'>
+      <div className='pt-[11.25rem] md:pt-[6.875rem] lg:pt-[8rem] bg-black'>
+        <div className="container bg-white">
+          <div className='relative overflow-hidden w-full flex flex-wrap justify-between'>
+            <div></div>
+            <div></div>
+            <div className='absolut top-0 bottom-0 uppercase w-[calc(50%-1.375rem)]'></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
