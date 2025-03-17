af09d575e6210cb235d4b5e829797c0a17e9142a
freewindias <freewindias6@gmail.com>
2025-03-17 14:13:07 +0530

    18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,19 @@
 
 import Image from "next/image";
 import heroImage from "@/assets/images/hero.jpg";
-import SplitType from "split-type";
-import { useAnimate, motion, stagger } from "motion/react";
-import { useEffect } from "react";
+import { motion } from "motion/react";
 import Button from "@/components/ui/Button";
 import Link from "next/link";
+import useTextRevealAnimation from "@/hooks/useTextRevealAnimation";
+import { useEffect } from "react";
 
 export default function Home() {
-  const [titleScope, titleAnimate] = useAnimate();
+  const { scope, entranceAnimation } = useTextRevealAnimation();
 
   useEffect(() => {
-    new SplitType(titleScope.current, {
-      types: "lines,words",
-      tagName: "span",
-    });
-    titleAnimate(
-      titleScope.current.querySelectorAll(".word"),
-      {
-        transform: "translateY(0)",
-      },
-      {
-        duration: 0.5,
-        delay: stagger(0.2),
-      }
-    );
-  }, [titleScope, titleAnimate]);
+    entranceAnimation();
+  },[entranceAnimation])
+
   return (
     <div className="h-screen !max-w-full flex items-center justify-center relative">
       <Image
@@ -39,7 +27,7 @@ export default function Home() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           className="text-5xl md:text-7xl lg:text-7xl font-light"
-          ref={titleScope}
+          ref={scope}
         >
           Every canvas is a journey all its own.
         </motion.h1>
