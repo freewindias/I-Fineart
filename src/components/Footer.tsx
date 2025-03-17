d7284991ceef711f06fdcff1400d51da34c284f4
freewindias <freewindias6@gmail.com>
2025-03-17 16:12:05 +0530

    24

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 import rodiasLogo from "@/assets/images/rodias.jpeg";
+import lLogo from '@/assets/icons/inewc.svg'
 
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
@@ -40,11 +41,11 @@ const Footer: FC = () => {
     <footer className="bg-stone-900 text-white sticky bottom-0 -z-0">
       <div className="container">
         <div className="section">
-          <div className="grid md:grid-cols-3 md:items-center pt-0">
-            <div className="md:col-span-2 ">
-              <div className="flex">
-                
-                <h2>Fineart</h2>
+          <div className="grid md:grid-cols-3 md:items-center">
+            <div className="md:col-span-2 mt-6">
+              <div className="flex items-center gap-2">
+                <Image src={lLogo} alt="Ilogo" height={150} width={150}></Image>
+                <h2 className=" text-7xl md:text-[100px]">Fineart</h2>
               </div>
               {/* <h2 className="text-3xl pt-6 font-extralight md:text-7xl lg:text-8xl">
                 Enough talk. Lets make something great together.
@@ -63,8 +64,8 @@ const Footer: FC = () => {
             </div>
           </div>
         </div>
-        <div className="mt-10 pb-10 border border-t-stone-700 border-l-0 border-r-0 border-b-0 ">
-          <div className="mt-4 md:flex md:justify-between md:items-center">
+        <div className="mt-5 md:mt-10 pb-6 border border-t-stone-700 border-l-0 border-r-0 border-b-0 ">
+          <div className="mt-0 md:mt-2 md:flex md:justify-between md:items-center">
             <p className="py-5 text-white/40 text-sm">
               Copyright &copy; RODIAS &bull; All rights reserved
             </p>
