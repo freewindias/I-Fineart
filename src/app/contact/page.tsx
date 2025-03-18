5b30a4bdf147398c03556d47dbdc995c0d215128
freewindias <freewindias6@gmail.com>
2025-03-18 17:30:58 +0530

    32

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,12 @@
-import React from 'react'
+import ContactForm from "@/components/ContactForm";
+// import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
+
+import React from "react";
 
 export default function Contact() {
   return (
-    <div className='mt-20'>
-      <div className='pt-[11.25rem] md:pt-[6.875rem] lg:pt-[8rem] bg-black'>
-        <div className="container bg-white">
-          <div className='relative overflow-hidden w-full flex flex-wrap justify-between'>
-            <div></div>
-            <div></div>
-            <div className='absolut top-0 bottom-0 uppercase w-[calc(50%-1.375rem)]'></div>
-          </div>
-        </div>
-      </div>
+    <div className="mt-20">
+      <ContactForm/>
     </div>
-  )
+  );
 }
