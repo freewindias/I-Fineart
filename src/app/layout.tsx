3953fa2f8f3338de00470c75cae989ba66eb83e3
freewindias <freewindias6@gmail.com>
2025-03-23 23:10:46 +0530

    44

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
+
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "I-Fineart",
