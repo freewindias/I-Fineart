56249b62ef05b7aa9418de73cf649d77ca7df518
freewindias <freewindias6@gmail.com>
2025-03-25 19:29:59 +0530

    50

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Admin() {
+  return (
+    <div className='mt-20'>Admin</div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import Footer from "@/components/Footer";
-
-import NavNew from "@/components/NavbarNew";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "I-Fineart",
@@ -18,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={"antialiased bg-black text-white"}>
-        <NavNew/>
+        {/* <NavNew/> */}
+        <Navbar/>
         <main className="relative z-10 min-h-[100vh] bg-black text-white">{children}</main>
         <Footer />
       </body>
